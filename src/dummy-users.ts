42a9f1528977b238363ec9afaef427ae2ad7f1ed
sayed123 <sayedmohameddev@gmail.com>
2024-11-26 02:49:17 -0800

    Resolved merge conflicts

---
+++ src/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'sayed mohamed sayed ',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'mohamed saad',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'kareem nasser',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'mukhtar mohamed ',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'ahmed samir',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'karam mohamed',
+    avatar: 'user-6.jpg',
+  },
+];
